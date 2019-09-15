42e51a9f17746a97ea8ecfbf1b948373c4789173
Alex <titosuperb@gmail.com>
2019-09-14 21:47:05 -0500

    Added javascript to make it usable. The difference days function doesn't work for dates with different years. The days left won't stop, it will turn negative eventually. Besides that its functional, and it works for me.

--- data_left/data_left.js
+++ data_left/data_left.js
@@ -4,23 +4,62 @@ Date.prototype.addDays = function(days) {
     date.setDate(date.getDate() + days);
     return date;
 }
-//
 
-// function differenceDays(date1, date2, limit){
-//   let dates = [];
-//   cntrd = new Date(date1.valueOf());
-//   dates[dates.length] = cntrd;
-//   cntr = 0;
-//   while(true){
-//     cntr +=1 ;
-//     cntrd.setDate(cntrd.getDate() + 1)
-//     dates[dates.length] = cntrd;
-//     if((dates.length == limit) || (cntrd == date2)){
-//       break
-//     }
-//   }
-//   return dates.length - 1
-// }
+function sum(nums){
+  cntr = 0;
+  for (var i = 0; i < nums.length; i++) {
+    cntr += nums[i];
+  }
+  return cntr
+}
+
+function mrd(d1, d2) {
+  ld1 = [d1.getFullYear() > d2.getFullYear(), d1.getMonth() > d2.getMonth(), d1.getDate() > d2.getDate()];
+  ld2 = [d2.getFullYear() > d1.getFullYear(), d2.getMonth() > d1.getMonth(), d2.getDate() > d1.getDate()];
+  for (var i = 0; i <= 2; i++) {
+    if (!(ld1[i]) && (ld2[i])){
+      return 1
+    } else if ((ld1[i]) && !(ld2[i])) {
+      return 0
+    } else {
+      if(i === 2){
+        return NaN
+      }
+    }
+  }
+}
+
+function gdn(d){
+  //Get date number
+  months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  days = sum(months.slice(0, d.getMonth())) + d.getDate();
+  if((d.getMonth() > 1) && ((d.getFullYear() % 4) == 0)){
+    return days + 1
+  } else {
+    return days
+  }
+}
+
+function differenceDays(date1, date2){
+  d1 = [date1, date2][mrd(date1, date2)];//More recent date
+  d2 = [date1, date2][(!mrd(date1, date2)) ? 1:0];
+
+  //Scenario 1: Two years apart or more
+  //Scenario 2: One year apart
+  //Scenario 1: Same years apart
+
+  dd1 = gdn(d1);
+  dd2 = gdn(d2);
+
+  yd = d1.getFullYear() - d2.getFullYear();
+  if(yd >= 2){
+
+  }else if (yd == 1) {
+
+  }else{
+    return dd1 - dd2
+  }
+}
 
 let planLength = localStorage.getItem("planLength");
 let lastRecharge = localStorage.getItem("lastRecharge");
@@ -35,12 +74,6 @@ if((planData === null) || (lastRecharge === null) || (planLength === null)){
   document.getElementById("max_data").innerHTML = "";
   document.getElementById("days_left").innerHTML = "Please enter the information of your plan.";
 } else {
-  console.log(planLength);
-
-  console.log(lastRecharge);
-  console.log(lastRechargeDate);
-  console.log(differenceDays(lastRechargeDate, today));
-  console.log(planData);
-
-  // console.log(lastRechargeDate.addDays(parseInt(planLength)))
+  document.getElementById("max_data").innerHTML = Math.floor((planData/planLength)*differenceDays(lastRechargeDate, today))+"Mb";
+  document.getElementById("days_left").innerHTML = planLength - differenceDays(lastRechargeDate, today) + " days left";
 }
